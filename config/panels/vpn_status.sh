2587a5788b044d0285e1311442fbbf9d61e24b98
Enrique Andrade González <nettinghxc@gmail.com>
2022-10-21 12:42:23 +0200

    Update vpn_status.sh

--- config/panels/vpn_status.sh
+++ config/panels/vpn_status.sh
@@ -3,8 +3,22 @@
 IFACE=$(/usr/sbin/ifconfig | grep tun0 | awk '{print $1}' | tr -d ':')
 
 if [ "$IFACE" = "tun0" ]; then
-        echo  "$(/usr/sbin/ifconfig tun0 | grep "inet " | awk '{print $2}')    "
+        echo -n ☠
+        echo -n "$(/usr/sbin/ifconfig tun0 | grep "inet " | awk '{print $2}')    "
         echo "$(/usr/sbin/ifconfig tun0 | grep "inet " | awk '{print $2}')" > /home/kali/.config/panels/ip_vpn
+	IP="$(/usr/sbin/ifconfig tun0 | grep 'inet ' | awk '{print $2}')"
+	if [ IP="" ]; then
+		IFACE=$(/usr/sbin/ifconfig | grep tun1 | awk '{print $1}' | tr -d ':')
+		if [ "$IFACE" = "tun1" ]; then
+			echo "$(/usr/sbin/ifconfig tun1 | grep 'inet ' | awk '{print $2}')" > /home/kali/.config/panels/ip_vpn
+			echo "$(/usr/sbin/ifconfig tun1 | grep 'inet ' | awk '{print $2}')    "
+		else
+			echo "[X] ERROR    "
+		fi
+	else
+		echo "$(/usr/sbin/ifconfig tun0 | grep "inet " | awk '{print $2}')" > /home/kali/.config/panels/ip_vpn
+	fi
 else
-        echo "     Disconnected    "
+        echo "    ⚠  Disconnected    "
 fi
+
